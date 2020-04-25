16f19bc1255f7c13eb5eb109840b42d3f35acc6b
mkvicala1 <57120699+mkvicala1@users.noreply.github.com>
2020-04-25 00:03:43 -0500

    Add files via upload

---
+++ Tournament.java
@@ -0,0 +1,19 @@
+package main;
+
+
+public class Tournament
+{
+    public void Tournament(String leagueName, String username)
+    {
+        String[] playerList = {};
+    }
+    public void Tournament(String username)
+    {
+        String[] playerList =  {};
+    }
+
+    public void generateBracket()
+    {
+        //Genreate, display, and 
+    }
+}
